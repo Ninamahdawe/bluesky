bf4b0363aa8fadef3a4348a4a534117b32221c2f
Sergio Vidales Perez <srvidales@gmail.com>
2023-09-25 13:48:30 -0700

    Implement get in game route

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,59 +1,13 @@
 const router = require('express').Router();
 const { Game } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-    try {
-        const gameData = await Game.findAll();
-        res.json(gameData);
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
-
-router.post('/', withAuth, async (req, res) => {
-    try {
-      const newProject = await Project.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newProject);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-
-router.post('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newProject);
+    const gameData = await Game.findAll();
+    res.json(gameData);
   } catch (err) {
     res.status(400).json(err);
-  }
-});
-
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const projectData = await Project.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
-      return;
-    }
-
-    res.status(200).json(projectData);
-  } catch (err) {
-    res.status(500).json(err);
   }
 });
 
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const gameRoutes = require('./gameRoutes');
 // // const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
+router.use('/game', gameRoutes);
 // // router.use('/projects', projectRoutes);
 
 module.exports = router;
